b3f72a5eb61423f1ab42250d927f91d00bf9b1e4
ZackChang <997571692@qq.com>
2022-02-21 20:46:15 +0800

    完善

--- dist/index.js
+++ dist/index.js
@@ -37,7 +37,21 @@ var Zack = {
 const data = {
   item1: 'bb',
   item2: 'cc'
-};
+}; // const jsx = Zack.createElement("ul", {
+//   className: "list"
+// }, Zack.createElement("li", {
+//   className: "item",
+//   style: {
+//     background: 'blue',
+//     color: 'pink'
+//   },
+//   onClick: () => alert(2)
+// }, "aa"), Zack.createElement("li", {
+//   className: "item"
+// }, data.item1, Zack.createElement("i", null, "xxx")), Zack.createElement("li", {
+//   className: "item"
+// }, data.item2));
+
 const jsx = Zack.createElement("ul", {
   className: "list"
 }, Zack.createElement("li", {
--- index.js
+++ index.js
@@ -38,20 +38,26 @@ const data = {
   item1: 'bb',
   item2: 'cc'
 };
-const jsx = Zack.createElement("ul", {
-  className: "list"
-}, Zack.createElement("li", {
-  className: "item",
-  style: {
-    background: 'blue',
-    color: 'pink'
-  },
-  onClick: () => alert(2)
-}, "aa"), Zack.createElement("li", {
-  className: "item"
-}, data.item1, Zack.createElement("i", null, "xxx")), Zack.createElement("li", {
-  className: "item"
-}, data.item2));
+// const jsx = Zack.createElement("ul", {
+//   className: "list"
+// }, Zack.createElement("li", {
+//   className: "item",
+//   style: {
+//     background: 'blue',
+//     color: 'pink'
+//   },
+//   onClick: () => alert(2)
+// }, "aa"), Zack.createElement("li", {
+//   className: "item"
+// }, data.item1, Zack.createElement("i", null, "xxx")), Zack.createElement("li", {
+//   className: "item"
+// }, data.item2));
+const jsx = <ul className="list">
+  <li className="item" style={{ background: 'blue', color: 'pink' }} onClick={() => alert(2)}>aa</li>
+  <li className="item">{data.item1}<i>xxx</i></li>
+  <li className="item">{data.item2}</li>
+</ul>;
+
 console.log('vdom: ', jsx); // schedule
 let nextFiberReconcileWork = null; // 下一个需要创建fiber的VDOM
 
